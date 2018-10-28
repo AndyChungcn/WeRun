7a42b0e3773ca0fc1e762b609e7ff6b85eda9026
AndyChung <andychung1005@gmail.com>
2018-10-29 07:57:46 +0800

    Add RealmSwift Cocoapod & Setup RunVC & LogVC & Current VC via Storyboard

---
+++ Podfile
@@ -0,0 +1,13 @@
+project 'WeRun.xcodeproj'
+
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'WeRun' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for WeRun
+  pod 'RealmSwift'
+
+end
---
+++ WeRun/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  WeRun
+//
+//  Created by 钟汇杭 on 2018/10/29.
+//  Copyright © 2018 钟汇杭. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
